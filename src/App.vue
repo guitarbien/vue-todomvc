1e789ef1c6fc01b1b75df1061a88c52f519f085e
guitarbien <guitarbien@gmail.com>
2018-08-12 13:39:34 +0800

    change main template to todomvc's
    template 標籤內即為 Vue 元件的 HTML 內容
    style 標籤可以用 src 來將 node 套件裡的 css 拉進來
    script 標籤要 export 物件給 main.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo"
+             autofocus autocomplete="off"
+             placeholder="What needs to be done?">
+    </header>
   </div>
 </template>
-
+<style src="todomvc-app-css/index.css"></style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- tests/e2e/specs/todomvc.js
+++ tests/e2e/specs/todomvc.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': browser => {
+  '應顯示程式標題與輸入欄位': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
       .waitForElementVisible('#app', 5000)
