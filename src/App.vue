a3a420bb2c4e7bda2c4f823280e5ef1025e2775c
guitarbien <guitarbien@gmail.com>
2018-08-12 13:46:21 +0800

    輸入欄位應該在輸入待辦事項後清空

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div id="app" class="todoapp">
-    <header class="header">
-      <h1>todos</h1>
-      <input class="new-todo"
-             autofocus autocomplete="off"
-             placeholder="What needs to be done?">
-    </header>
-  </div>
+    <div id="app" class="todoapp">
+        <header class="header">
+            <h1>todos</h1>
+            <input class="new-todo"
+                   autofocus autocomplete="off"
+                   placeholder="What needs to be done?"
+                   v-model="newTodo"
+                   @keyup.enter="addTodo">
+        </header>
+    </div>
 </template>
 <style src="todomvc-app-css/index.css"></style>
 <script>
-  export default {};
+  export default {
+    data() {
+      return {
+        newTodo: ''
+      };
+    },
+    methods: {
+      addTodo() {
+        this.newTodo = '';
+      }
+    }
+  };
 </script>
--- tests/e2e/specs/todomvc.js
+++ tests/e2e/specs/todomvc.js
@@ -10,5 +10,15 @@ module.exports = {
       .assert.containsText('h1', 'todos')
       .assert.elementPresent('.new-todo')
       .end()
+  },
+  '輸入欄位應該在輸入待辦事項後清空' (browser) {
+    const todo = 'This is new todo';
+
+    browser
+      .url(process.env.VUE_DEV_SERVER_URL)
+      .waitForElementVisible('#app', 5000)
+      .setValue('.new-todo', [todo, browser.Keys.ENTER])
+      .assert.value('.new-todo', '')
+      .end();
   }
 }
