2065a1570aded54280a666773d502ada78a830aa
guitarbien <guitarbien@gmail.com>
2018-08-12 22:11:14 +0800

    arrange 用 localStorage 準備好測試資料

--- tests/unit/specs/store.spec.js
+++ tests/unit/specs/store.spec.js
@@ -4,5 +4,9 @@ chai.should();
 
 describe('Store', () => {
   it('應可從 localStorage 中取得資料', () => {
+    // arrange
+    const STORAGE_KEY = 'vue-todomvc';
+    const expected = ['test'];
+    localStorage.setItem(STORAGE_KEY, '["test"]');
   });
 });
