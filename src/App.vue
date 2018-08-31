264eb71c0108844739b6bba51a5ddfb1f11a552a
guitarbien <guitarbien@gmail.com>
2018-08-31 16:58:37 +0800

    filter功能

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,10 @@
             <todo-input @add-todo="addTodo"></todo-input>
         </header>
         <section class="main" v-show="todos.length" v-cloak>
+            <input class="toggle-all" type="checkbox" data-com.agilebits.onepassword.user-edited="yes">
+            <label for="toggle-all">Mark all as complete</label>
             <ul class="todo-list">
-                <li class="todo" :class="{ completed: todo.completed }" v-for="(todo, index) in todos" :key="index">
+                <li class="todo" :class="{ completed: todo.completed }" v-for="(todo, index) in loopFilterTodos" :key="index">
                     <div class="view">
                         <input class="toggle" type="checkbox" v-model="todo.completed" />
                         <label>{{ todo.title }}</label>
@@ -15,6 +17,21 @@
                 </li>
             </ul>
         </section>
+        <footer class="footer" style="display: block;">
+            <span class="todo-count"><strong v-if="AllTodosCount > 0">{{ ActiveTodosCount }}</strong> {{ howManyTodo }} left</span>
+            <ul class="filters">
+                <li>
+                    <a href="#/" @click="changeSelect('all')" :class="{ selected: isSelected('all') }">All</a>
+                </li>
+                <li>
+                    <a href="#/active" @click="changeSelect('active')" :class="{ selected: isSelected('active') }">Active</a>
+                </li>
+                <li>
+                    <a href="#/completed" @click="changeSelect('completed')" :class="{ selected: isSelected('completed') }">Completed</a>
+                </li>
+            </ul>
+            <button v-if="CompletedTodosCount > 0" @click="removeCompletedTodos" class="clear-completed" style="display: block;">Clear completed</button>
+        </footer>
     </div>
 </template>
 <style src="todomvc-app-css/index.css"></style>
@@ -25,7 +42,8 @@
     components: { TodoInput },
     data() {
       return {
-        todos: []
+        todos: [],
+        visibilityFlag: 'all'
       };
     },
     methods: {
@@ -37,6 +55,62 @@
       },
       removeTodo(index) {
         this.todos.splice(index, 1);
+      },
+      removeCompletedTodos() {
+        this.todos.forEach((item, index) => {
+          if (item.completed === true) {
+            this.removeTodo(index);
+          }
+        });
+      },
+      changeSelect(input) {
+        this.visibilityFlag = input;
+      },
+      isSelected(flag) {
+        return this.visibilityFlag === flag;
+      }
+    },
+    computed: {
+      ActiveTodos() {
+        return this.todos.filter((item) => item.completed === false);
+      },
+      /**
+       * @return {number}
+       */
+      ActiveTodosCount() {
+        return this.ActiveTodos.length;
+      },
+      CompletedTodos() {
+        return this.todos.filter((item) => item.completed === true);
+      },
+      /**
+       * @return {number}
+       */
+      CompletedTodosCount() {
+        return this.CompletedTodos.length;
+      },
+      AllTodos() {
+        return this.todos;
+      },
+      /**
+       * @return {number}
+       */
+      AllTodosCount() {
+        return this.AllTodos.length;
+      },
+      howManyTodo() {
+        return this.ActiveTodosCount === 1 ? 'item' : 'items';
+      },
+      loopFilterTodos() {
+        if (this.visibilityFlag === 'all') {
+          return this.AllTodos;
+        }
+        if (this.visibilityFlag === 'completed') {
+          return this.CompletedTodos;
+        }
+        if (this.visibilityFlag === 'active') {
+          return this.ActiveTodos;
+        }
       }
     }
   };
